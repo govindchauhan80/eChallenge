9b5a7c24a170b338f093872fa042b826cb5af6ca
unknown <versata.app.test@gmail.com>
2015-05-23 00:30:34 +0530

    code setup

---
+++ EChallenge/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EChallenge.Startup))]
+namespace EChallenge
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
