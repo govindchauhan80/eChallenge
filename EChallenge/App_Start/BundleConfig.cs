80b0372c02fb4941067f5e3ca76b3b36d70382df
govindchauhan80 <chauhangovind3@gmail.com>
2015-06-25 00:44:27 +0530

    jqurery changes

--- EChallenge/App_Start/BundleConfig.cs
+++ EChallenge/App_Start/BundleConfig.cs
@@ -22,7 +22,8 @@ namespace EChallenge
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/respond.js",
+                      "~/Content/js/jquery.bxslider.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
