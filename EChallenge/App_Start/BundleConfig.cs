4106c1dc42344487d3dc17d0773354eb17204e7b
govindchauhan80 <chauhangovind3@gmail.com>
2015-06-25 00:11:42 +0530

    jquery fixes

--- EChallenge/App_Start/BundleConfig.cs
+++ EChallenge/App_Start/BundleConfig.cs
@@ -9,7 +9,8 @@ namespace EChallenge
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/responsiveslides.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
